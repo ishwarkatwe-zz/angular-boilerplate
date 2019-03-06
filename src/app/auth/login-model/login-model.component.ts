9a0cb44a0fc9472536933614516bf13081ac7f4c
ishwarkatwe <ishwar.mk@molecularconnections.com>
2019-03-06 17:02:19 +0530

    'init'

---
+++ src/app/auth/login-model/login-model.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-login-model',
+  templateUrl: './login-model.component.html',
+  styleUrls: ['./login-model.component.scss']
+})
+export class LoginModelComponent implements OnInit {
+  showModel: boolean = false;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
